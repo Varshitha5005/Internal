b3af5c6053377c1de321ad4650872827e448c0de
Varshitha5005 <245122748010@mvsrec.edu.in>
2025-06-08 17:53:38 +0530

    Update Java2.java

--- Java2.java
+++ Java2.java
@@ -3,6 +3,6 @@ class Java2
 public static void main(String[] args)
 {
 System.out.println("Name:"+args[0]);
-System.out.println("Roll No:"+Integer.parseInt(args[1]);
+System.out.println("Roll No:"+Integer.parseInt(args[1]));
 }
 }
