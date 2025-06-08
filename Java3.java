2b44b4572cd49c94188ccaa3bbe0d82a4c26185d
Varshitha5005 <245122748010@mvsrec.edu.in>
2025-06-08 18:00:28 +0530

    'j3'

---
+++ Java3.java
@@ -0,0 +1,14 @@
+class Java3
+{
+public static void main(String[] args)
+{
+int sum=0,avg=0;
+for(int i=1;i<=10;i++)
+{
+sum+=i;
+}
+avg=sum/10;
+System.out.println("sum"+sum);
+System.out.println("Avg"+avg);
+}
+}
